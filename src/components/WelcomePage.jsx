329dbc90981154bb9f86ffee5ff312ba9031dc45
Henrique Junqueira <henriquecamposj@gmail.com>
2024-03-18 08:18:06 -0300

    Added welcome and login pages

---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const WelcomePage = (props) => {
+  const { loggedIn, email } = props;
+  const navigate = useNavigate();
+
+  const onButtonClick = () => {
+    // You'll update this function later
+  };
+
+  return (
+    <div className="mainContainer">
+      <div className={"titleContainer"}>
+        <div>Welcome to Arena, rebyata!</div>
+      </div>
+      <div>This is the home page.</div>
+      <div className={"buttonContainer"}>
+        <input
+          className={"inputButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={loggedIn ? "Logout" : "Login"}
+        />
+        {loggedIn ? <div>Your email address is {email}</div> : <div />}
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
