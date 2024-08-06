1e2d0a057ec98af19eab666bb0ecdce41c2704df
mateus321 <mateuspereirafernandes@gmail.com>
2024-08-06 10:29:02 -0300

    About check

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,8 +8,23 @@ function Contact() {
     return (
         <div>
             <Header />
-
+            <div className="mainContainer">
+                <div className="containerAbout">
+                    <div className="imagem">
+                        <img src={logo} alt={"logo"} />
+                    </div>
+                    <div className="texto">
+                        <h1>Contact Us</h1>
+                        <p>
+                            If you have any questions or suggestions, please contact us through the email below:
+                        </p>
+                        <h1>Email</h1>
+                        
+                    </div>
+                </div>
+            </div>
             <Footer />
+
         </div>
     );
 }
