596a68282fb82774dc41a1b9e3fccbcddf9853c1
RafaelColi <rafacolialves@gmail.com>
2024-08-08 02:19:56 -0300

    Remoção de código desnecessário

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage";
 import LoginPage from "./components/LoginPage";
 import "./App.css";
-import { useEffect, useState } from "react";
 import RegisterPage from "./components/RegisterPage";
 import ProblemPage from "./components/ProblemPage";
 import ProblemList from "./components/ProblemList";
 import Services from "./components/Services";
 
 function App() {
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [email, setEmail] = useState("");
 
     return (
         <div className="App">
--- src/components/Storage.jsx
+++ src/components/Storage.jsx
@@ -1,25 +1,3 @@
-import { useState, useEffect } from "react";
-
-function getStorageValue(key, defaultValue) {
-  // getting stored value
-  const saved = localStorage.getItem(key);
-  const initial = saved;
-  return initial || defaultValue;
-}
-
-export const useLocalStorage = (key, defaultValue) => {
-  const [value, setValue] = useState(() => {
-    return getStorageValue(key, defaultValue);
-  });
-
-  useEffect(() => {
-    // storing input name
-    localStorage.setItem(key, value);
-  }, [key, value]);
-
-  return [value, setValue];
-};
-
 export const setDataStorage = (key, value) => {
     localStorage.setItem(key, value);
 }
